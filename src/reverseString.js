442b7ec593edffaadc63f806713439413af449c9
Gabriel Sena <gabesenacom@gmail.com>
2021-07-22 20:37:24 -0300

    Change reverseString.js to use useful methods

--- src/reverseString.js
+++ src/reverseString.js
@@ -1,10 +1,3 @@
 export default function reverseString (string) {
-  if (!string) return null
-  let reverse = ''
-
-  while (reverse.length !== string.length) {
-    reverse += string[string.length - reverse.length - 1]
-  }
-
-  return reverse
+  return string ? [...string].reverse().join('') : null
 }
