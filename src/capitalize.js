aad171dc53ba24c6de163c17e5a67f00c448b6df
Gabriel Sena <gabesenacom@gmail.com>
2021-07-22 20:36:55 -0300

    Change capitalize.js to use useful methods

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,12 +1,3 @@
 export default function capitalize (string) {
-  if (!string) return undefined
-  let str = ''
-  while (str.length != string.length) {
-    if (str.length == 0) {
-      str += string[0].toUpperCase()
-    } else {
-      str += string[str.length]
-    }
-  }
-  return str
+  return string ? string[0].toUpperCase() + string.slice(1) : null
 }
